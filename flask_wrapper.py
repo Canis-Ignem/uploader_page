452da8cc0560c413ebf44184e5a029bbec2a5b52
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-06-15 10:35:54 +0200

    trial for dilan

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__, template_folder="./templates")
 
-UPLOAD_FOLDER = "/home/bridgestone/Documents/"
+UPLOAD_FOLDER = "/home/dilan/"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
