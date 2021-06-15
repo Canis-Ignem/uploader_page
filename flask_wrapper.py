fb43d06c5d07bbdee1f6c056b0e7c57cfe05e578
Canis-Ignem <jonperezetxebarria@gmail.com>
2021-06-15 10:31:13 +0200

    wrong pth

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__, template_folder="./templates")
 
-UPLOAD_FOLDER = "~/Documents/"
+UPLOAD_FOLDER = "~/Downloads/"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
