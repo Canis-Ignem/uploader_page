9fc88b78b6f05ecf56c993b56f90f3c4e90852f1
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-06-20 23:43:12 +0200

    db init

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from md5 import md5
 
 engine = create_engine("mysql+pymysql://phpmyadmin:Estocolmo597B@localhost/")
 conn = engine.connect()
-#conn.cursor().execute("CREATE TABLE users(user text, md5 text PRIMARY KEY)")
+conn.execute("CREATE TABLE users(user text, md5 text PRIMARY KEY)")
 #conn.cursor().execute("INSERT INTO users VALUES('keystone','6458f3bfa6486a2be61b9fb6f37645c8')")
 
 def get_sum(user):
@@ -17,10 +17,10 @@ def add_user(user, pas):
     try:
         
         md5_sum = md5(pas)
-        conn.cursor().execute("INSERT INTO users VALUES('{}','{}')".format(user, md5_sum))
+        conn.execute("INSERT INTO users VALUES('{}','{}')".format(user, md5_sum))
         conn.commit()
         return True
     except:
         return False
     
-print(get_sum("keystone"))
+print(add_user("test", "a"))
