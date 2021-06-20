470c4999d02ab0323537c544e1d0a49d899ac548
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-06-20 23:40:27 +0200

    db update

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from md5 import md5
 
 
-engine = create_engine("mysql+pymysql://phpmyadmin:Estocolmo597B@localhost/phpmyadmin?host=localhost?port=3306")
+engine = create_engine("mysql+pymysql://phpmyadmin:Estocolmo597B@localhost/")
 conn = engine.connect()
 #conn.cursor().execute("CREATE TABLE users(user text, md5 text PRIMARY KEY)")
 #conn.cursor().execute("INSERT INTO users VALUES('keystone','6458f3bfa6486a2be61b9fb6f37645c8')")
