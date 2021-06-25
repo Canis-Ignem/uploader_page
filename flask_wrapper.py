c962275e4e499337279927b72837001c6eed2709
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-06-25 11:09:38 +0200

    upload to user

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -8,7 +8,10 @@ app = Flask(__name__, template_folder="./templates")
 
 uploads_dir = os.path.join(app.instance_path, 'uploads')
 #os.popen("sudo -S %s"%("mkdir {}".format(uploads_dir)), 'w').write('Estocolmo597B')
-os.popen("sudo -S %s"%("mkdir /home/keystone/testing"), 'w').write('Estocolmo597B')
+passwd = ""
+with open("pass",'r') as p:
+    passwd = p.read()
+os.popen("sudo -S %s"%("mkdir /home/keystone/testing"), 'w').write(passwd)
 LOGGED_USER = ""
 
 @app.route("/")
@@ -26,7 +29,7 @@ def get_file():
                 user = request.form["user"]
                 f = request.files["uploaded_file"]
                 #return os.path.join("/home/{}/".format(user), secure_filename(f.name))
-                f.save( "/home/keystone/"+secure_filename(f.filename))
+                f.save( "/home/{}/uploads/".format(user)+secure_filename(f.filename))
                 return "a"
                 return render_template("index.html")
     except:
