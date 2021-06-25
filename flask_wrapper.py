f7a6fd9c5ea084646aed43755a6787f0cfa79503
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-06-25 10:56:54 +0200

    test4

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -25,7 +25,7 @@ def get_file():
                 
                 user = request.form["user"]
                 f = request.files["uploaded_file"]
-                return os.path.join("/home/{}/".format(user), secure_filename(f.name))
+                return os.path.join("/home/{}/".format(user), f.name)
                 f.save(os.path.join("/home/{}/".format(LOGGED_USER), secure_filename(f.name)))
                 return "a"
                 return render_template("index.html")
