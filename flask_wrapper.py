f1adbabdfd1153319badc17bcf4ec313ac249f1c
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-06-29 00:09:27 +0200

    casting to string

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -108,7 +108,8 @@ def launch_jupyter():
         
     response = os.popen("cd /home/{} \n jupyter-notebook --no-browser".format(user)).readlines()
     #response = os.popen("jupyter-notebook list").readlines()
-    return response.split(":")
+    lines = str(response).split(":")
+    return lines[1]
 
 if __name__ == "__main__":
     app.run("192.168.1.44")
