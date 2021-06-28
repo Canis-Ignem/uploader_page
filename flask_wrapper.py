f5292545a1a7d86a6a003dff9bd4bbe8b334c595
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-06-29 00:14:36 +0200

    t

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -106,9 +106,9 @@ def launch_jupyter():
     with open("pass",'r') as p:
         passwd = p.read()
         
-    response = os.popen("cd /home/{} \n jupyter-notebook --no-browser".format(user)).readlines()
-    #response = os.popen("jupyter-notebook list").readlines()
-    return response[0]
+    os.popen("cd /home/{} \n jupyter-notebook --no-browser".format(user)).readlines()
+    response = os.popen("jupyter-notebook list").readlines()
+    return response
 
 if __name__ == "__main__":
     app.run("192.168.1.44")
