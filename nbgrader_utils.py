69ac6554ee88c0b3c601effefd3e690106441782
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-02 19:01:24 +0200

    started nbgrader

---
+++ nbgrader_utils.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+passwd = ""
+with open("pass",'r') as p:
+        passwd = p.read()
+
+def create_batch(filename):
+    with open(filename, 'r') as f:
+        
+        os.popen("mkdir /home/keystone/Autograding/{}".format(f.name)).write(passwd)
+        
+        students = pd.read_csv(filename)
+        
+        for std in students:
+            
+            os.popen("mkdir /home/keystone/Autograding/{}/{}".format(f.name, std['email'])).write(passwd)
+            
+            
+            
