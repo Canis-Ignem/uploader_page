2fbcbeb15d7adf9c6d1e139a572ccd12e59466e0
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-02 19:55:21 +0200

    file moving

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -35,7 +35,6 @@ def get_file():
                 with open("pass",'r') as p:
                     passwd = p.read()
                 os.popen("sudo -S %s"%("mkdir /home/{}/uploads".format(user)), 'w').write(passwd)
-                return "mv {} /home/{}/uploads".format(f.filename, user)
                 os.popen("sudo -S %s"%("mv \"{}\" /home/{}/uploads".format(f.filename, user)), 'w').write(passwd)
                 
                 return render_template("index.html",  name = user)
