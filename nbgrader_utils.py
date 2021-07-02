1016da9ee1e5521106f212ad9181688e4fc63321
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-02 19:09:47 +0200

    t

--- nbgrader_utils.py
+++ nbgrader_utils.py
@@ -11,10 +11,10 @@ def create_batch(filename):
         
         students = pd.read_csv(filename, header=1)
         
-        for std in students:
+        for std in students['email']:
             
             print(std)
-            os.popen("mkdir /home/keystone/Autograding/{}/{}".format(f.name, std['email']))
+            os.popen("mkdir /home/keystone/Autograding/{}/{}".format(f.name, std))
             
             
 create_batch("AI-May21.csv")
