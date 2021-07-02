992095b2689168e769a06f2b51899e71ef167c53
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-02 19:06:49 +0200

    t

--- nbgrader_utils.py
+++ nbgrader_utils.py
@@ -7,13 +7,14 @@ with open("pass",'r') as p:
 def create_batch(filename):
     with open(filename, 'r') as f:
         
-        os.popen("mkdir /home/keystone/Autograding/{}".format(f.name)).write(passwd)
+        os.popen("mkdir /home/keystone/Autograding/{}".format(f.name))
         
         students = pd.read_csv(filename)
         
         for std in students:
             
-            os.popen("mkdir /home/keystone/Autograding/{}/{}".format(f.name, std['email'])).write(passwd)
+            print(std)
+            os.popen("mkdir /home/keystone/Autograding/{}/{}".format(f.name, std[1]))
             
             
 create_batch("AI-May21.csv")
