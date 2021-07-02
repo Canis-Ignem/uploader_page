931ca7fbf1a413287818db4635591504ada473c3
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-02 18:32:29 +0200

    stupid update

--- db.py
+++ db.py
@@ -4,10 +4,11 @@ from md5 import md5
 
 passwd = ""
 with open("pass",'r') as p:
-	passwd = p.read()
+        passwd = p.read()
 
 engine = create_engine("mysql+pymysql://phpmyadmin:{}@localhost:3306/phpmyadmin".format(passwd[:-1]))
 conn = engine.connect()
+
 #conn.execute("CREATE TABLE users(user varchar(32) PRIMARY KEY, md5 text )")
 #conn.cursor().execute("INSERT INTO users VALUES('keystone','6458f3bfa6486a2be61b9fb6f37645c8')")
 
