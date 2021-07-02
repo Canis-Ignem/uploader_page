8fc474e0509abbed4d6f47a63789e99aeeb6873b
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-02 19:26:20 +0200

    creating ex folders

--- nbgrader_utils.py
+++ nbgrader_utils.py
@@ -7,7 +7,7 @@ with open("pass",'r') as p:
 def create_batch(filename):
     with open(filename, 'r') as f:
         
-        os.popen("mkdir /home/keystone/Autograding/{}".format(f.name))
+        os.popen("mkdir /home/keystone/Autograding/{}".format(f.name[:-4]))
         
         students = pd.read_csv(filename, header=0)
         
@@ -17,6 +17,10 @@ def create_batch(filename):
             os.popen("mkdir /home/keystone/Autograding/{}/{}".format(f.name, std[1]))
             
             
-create_batch("AI-May21.csv")
-            
-            
+def create_ex(exercise, batch):
+    
+    for fldr in os.listdir("mkdir /home/keystone/Autograding/{}".format(batch)):
+        
+        os.popen("mkdir /home/keystone/Autograding/{}/{}/{}".format(batch,fldr,exercise))
+        
+create_ex("ml1", "AI-May21")      
