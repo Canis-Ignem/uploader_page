cfe958df074fb985c493b6bad5a39023b84b1201
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-04 00:12:55 +0200

    testing db

--- db.py
+++ db.py
@@ -19,7 +19,7 @@ def add_user(user, pas, email, DoB, country_of_residence, batch, gender ):
     try:
         
         md5_sum = md5(pas)
-        conn.execute("INSERT INTO users VALUES('{}','{}','{}','{}','{}','{}','{}')".format(user, md5_sum, batch, email, gender, country_of_residence, DoB))
+        conn.execute("INSERT INTO users (user, md5, batch, email, gender, country_of_residence, DoB) VALUES('{}','{}','{}','{}','{}','{}','{}')".format(user, md5_sum, batch, email, gender, country_of_residence, DoB))
         return True
     except:
         return True
@@ -45,3 +45,5 @@ def get_email(user):
     
     email = res.fetchone()
     return str(email[0])
+
+print(add_user("test", "a", 'a',"1997-08-13", 'a', 'AI-May21', 'Male' ))
