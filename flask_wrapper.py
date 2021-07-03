453d206d0d5db41a9e3fb12d42156fc387745f68
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-03 19:35:07 +0200

    grading included

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -6,7 +6,8 @@ from md5 import md5
 import db
 from subprocess import Popen, list2cmdline
 import re
-
+import sqlite3 as sql
+import pandas as pd
 app = Flask(__name__, template_folder="./templates")
 
 uploads_dir = os.path.join(app.instance_path, 'uploads')
@@ -63,8 +64,11 @@ def nbgrader_ex():
                 os.popen("sudo -S %s"%("mkdir /home/keystone/Autograding/{}/submitted/{}/{}".format(batch, email,secure_filename(f.filename)[:-6] )), 'w').write(passwd)
                 os.popen("sudo -S %s"%("mv {} /home/keystone/Autograding/{}/submitted/{}/{}".format(f.filename,batch, email,secure_filename(f.filename)[:-6] )), 'w').write(passwd)
                 
-                if os.path.exists("/home/keystone/Autograding/{}/submitted/{}/{}/{}".format(batch, email,secure_filename(f.filename)[:-6],f.filename  )):
-                    return render_template("index.html", name = user,  correct = "File uploaded correctly")
+                if os.path.isdir("/home/keystone/Autograding/{}/submitted/{}/{}".format(batch, email,secure_filename(f.filename)[:-6],f.filename  )):
+                    
+                    os.popen("cd  /home/keystone/Autograding/{} \n nbgrader autograde --student {} --assignment ml1 ".format(email, secure_filename(f.filename)[:-6]))
+                    grade = get_grade(email, secure_filename(f.filename)[:-6])
+                    return render_template("index.html", name = user,  correct = grade)
                 else:
                     return render_template("index.html", name = user,  correct = "File failed upload")
                 
@@ -86,6 +90,7 @@ def login():
             if db.get_sum(user) == md5(request.form["psw"]):
                 
                 session['uname'] = user
+                session['email'] = db.get_email(user)
                 os.popen("cd /home/{} \n source /home/anaconda3/bin/activate \n jupyter-notebook --no-browser ".format(user))
                 return render_template("index.html", name = user, correct = '' )
                 
@@ -95,6 +100,36 @@ def login():
             
     except:
         return "Something went wrong"
+
+@app.route("/get_grade")
+def get_grade(email,ex):
+    
+    try:
+        con = sql.connect("/home/jon/gradebook.db")
+        
+        q1 = "SELECT id FROM assignment where name ='{}'".format(ex)
+        ass_id = pd.read_sql_query( q1 , con).values[0][0]
+        q2 = "Select id from submitted_assignment where student_id = '{}' and assignment_id = '{}'".format(ex,ass_id)
+        nb_id = pd.read_sql_query( q2 , con).values[0][0]
+
+        q3 = "Select id from submitted_notebook where assignment_id = '{}'".format(nb_id)
+        nb_id = pd.read_sql_query( q3 , con).values[0][0]
+
+        q4 = "Select auto_score,cell_id from grade where notebook_id = '{}'".format(nb_id)
+        grades = pd.read_sql_query( q4 , con)
+
+        cell_list = grades['cell_id'].values.tolist()
+        as_str = ','.join("\'"+str(cell_list[i])+ "\'"  for i in range(len(cell_list)))
+        q5 = "Select max_score from grade_cells where id IN ({})".format( as_str )
+        max_score = pd.read_sql_query( q5 , con)
+        #print(max_score['max_score'].sum())
+       
+        report = " The student:  {} \n Assigment:   {} \n Total marks: {}/{}".format(email,ex,grades['auto_score'].sum(),max_score['max_score'].sum())
+        #print(report)
+        return report
+    except:
+        #print("No submission for that student")
+        return "No submission for this student"
     
 
 @app.route("/register")
@@ -118,6 +153,7 @@ def sign_in():
 
             if db.add_user(user,request.form["psw2"], email, DoB, country, batch, gender):
                 session['uname'] = user
+                session['email'] = email
                 os.popen("cd /home/{} \n source /home/anaconda3/bin/activate \n jupyter-notebook --no-browser ".format(user))
                 return render_template("index.html", session['uname'])
             else:
