b11ebb5f2903c35e0c1805e0b828ff9eb8f56e1b
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-03 18:38:54 +0200

    typo fix

--- nbgrader_utils.py
+++ nbgrader_utils.py
@@ -7,21 +7,21 @@ with open("pass",'r') as p:
 def create_batch(filename):
     with open(filename, 'r') as f:
         
-        os.popen("mkdir /home/keystone/Autograding/{}/sumbitted".format(f.name[:-4]))
+        os.popen("mkdir /home/keystone/Autograding/{}/submitted".format(f.name[:-4]))
         
         students = pd.read_csv(filename, header=0)
         
         for std in students.values:
             
             print(std)
-            os.popen("mkdir /home/keystone/Autograding/{}/sumbitted/{}".format(f.name[:-4], std[1]))
+            os.popen("mkdir /home/keystone/Autograding/{}/submitted/{}".format(f.name[:-4], std[1]))
             
             
 def create_ex(exercise, batch):
     
-    for fldr in os.listdir("/home/keystone/Autograding/{}/sumbitted".format(batch)):
+    for fldr in os.listdir("/home/keystone/Autograding/{}/submitted".format(batch)):
         
-        os.popen("mkdir /home/keystone/Autograding/{}/sumbitted/{}/{}".format(batch,fldr,exercise))
+        os.popen("mkdir /home/keystone/Autograding/{}/submitted/{}/{}".format(batch,fldr,exercise))
 
 create_batch("AI-May21.csv")
 create_ex("ml1", "AI-May21")
