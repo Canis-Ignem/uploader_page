50ac324a23d9419350088a1cd6e3b7c4fb0fb2b3
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-03 19:43:17 +0200

    email bugfix

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -110,7 +110,7 @@ def get_grade(email,ex,batch):
         q1 = "SELECT id FROM assignment where name ='{}'".format(ex)
         ass_id = pd.read_sql_query( q1 , con).values[0][0]
         
-        q2 = "Select id from submitted_assignment where student_id = '{}' and assignment_id = '{}'".format(ex,ass_id)
+        q2 = "Select id from submitted_assignment where student_id = '{}' and assignment_id = '{}'".format(email,ass_id)
         nb_id = pd.read_sql_query( q2 , con).values[0][0]
         return nb_id
         q3 = "Select id from submitted_notebook where assignment_id = '{}'".format(nb_id)
