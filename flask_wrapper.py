a2d7b63dd6100f3dd6e689c10695c12d6c8c6a02
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-04 21:38:24 +0200

    db test2

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -118,10 +118,10 @@ def get_grade(email,ex,batch):
         
         q1 = "SELECT id FROM assignment where name ='{}'".format(ex)
         ass_id = pd.read_sql_query( q1 , con).values[0][0]
-        return 1,1
+        
         q2 = "Select id from submitted_assignment where student_id = '{}' and assignment_id = '{}'".format(email,ass_id)
         nb_id = pd.read_sql_query( q2 , con).values[0][0]
-        
+        return 1,1
         q3 = "Select id from submitted_notebook where assignment_id = '{}'".format(nb_id)
         nb_id = pd.read_sql_query( q3 , con).values[0][0]
 
