528705ac2f36b2b4d480771293812fca0c9ef781
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-04 21:34:24 +0200

    test

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -68,7 +68,7 @@ def nbgrader_ex():
                 os.popen("sudo -S %s"%("mv {} /home/keystone/Autograding/{}/submitted/{}/{}".format(f.filename,batch, email,secure_filename(f.filename)[:-6] )), 'w').write(passwd)
                 time.sleep(2)
                 if os.path.isfile("/home/keystone/Autograding/{}/submitted/{}/{}/{}".format(batch, email,secure_filename(f.filename)[:-6],secure_filename(f.filename) )):
-                    
+                    return "a"
                     os.popen("cd  /home/keystone/Autograding/{} \n conda activate nbg \n nbgrader autograde --student {} --assignment {} ".format(batch, email, secure_filename(f.filename)[:-6]))
                     grade, max_score = get_grade(email, secure_filename(f.filename)[:-6], batch)
                     
