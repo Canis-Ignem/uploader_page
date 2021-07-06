5273a20af3c4d07d0e9b8386bd2e32bcf1bd0cdd
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-06 14:47:37 +0200

    no sudo

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -106,7 +106,7 @@ def login():
                 with open("pass",'r') as p:
                     passwd = p.read()
                 
-                os.popen("cd /home/keystone/{} \n source /home/anaconda3/bin/activate \n sudo jupyter notebook --allow-root ".format(user), 'w').write(passwd)
+                os.popen("cd /home/keystone/{} \n source /home/anaconda3/bin/activate \n jupyter-notebook --allow-root ".format(user), 'w').write(passwd)
                 return render_template("index.html", name = user, correct = '' )
                 
             else:
